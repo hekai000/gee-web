8d7c4c7c1ddd27372f876b53b2627c5c0d42f751
hekai000 <675974119@qq.com>
2025-02-13 11:44:04 +0800

    hk commit feature)增加第3天trie树，路由注册，路由查询，参数字典功能代码

--- day3-router/main.go
+++ day3-router/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"gee-web/day2-context/gee"
+	"gee-web/day3-router/gee"
 	"net/http"
 )
 
 func main() {
 	r := gee.New()
-
 	r.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
@@ -17,11 +16,13 @@ func main() {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
